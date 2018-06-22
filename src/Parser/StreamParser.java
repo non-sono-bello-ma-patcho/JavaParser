df63b8c8bf26947ceed09b3b8776a89ca634731e
PageFaultHanler <alex.savona97@gmail.com>
2018-06-22 14:51:53 +0200

    little change

--- src/Parser/StreamParser.java
+++ src/Parser/StreamParser.java
@@ -9,7 +9,7 @@ Prog ::= StmtSeq 'EOF'
  StmtSeq ::= Stmt (';' StmtSeq)?
  Stmt ::= 'var'? ID '=' Exp | 'print' Exp |  'for' ID ':' Exp '{' StmtSeq '}' | if (Exp) {StmtSeq} (else {StmtSeq})? | do {StmtSeq} while (Exp)
  ExpSeq ::= Exp (',' ExpSeq)?
- Exp ::= Add ('::' Exp)? | Exp && Exp | Exp == Exp | ! Exp | opt Exp | empty Exp | def Exp | get Exp  attenzione : aggiunta io -->|true |false|binaryExpr ( per questa farei una regex)
+ Exp ::= Add ('::' Exp)? | Exp && Exp | Exp == Exp | ! Exp | opt Exp | empty Exp | def Exp | get Exp |true |false|binaryExpr
  Add ::= Mul ('+' Mul)*
  Mul::= Atom ('*' Atom)*
  Atom ::= '-' Atom | '[' ExpSeq ']' | NUM | ID | '(' Exp ')'
--- src/Parser/StreamTokenizer.java
+++ src/Parser/StreamTokenizer.java
@@ -24,10 +24,14 @@ public class StreamTokenizer implements Tokenizer {
 		final String identRegEx = "([a-zA-Z][a-zA-Z0-9]*)"; // group 1
 		final String numRegEx = "(0|[1-9][0-9]*)"; // group 2
 		final String skipRegEx = "(\\s+|//.*)"; // group 3
-		final String symbolRegEx = "\\+|\\*|==|=|&&|\\(|\\)|;|,|\\{|\\}|-|::|:|\\[|\\]"; /*modified by me */
+		final String symbolRegEx = "\\+|\\*|!|==|=|&&|\\(|\\)|;|,|\\{|\\}|-|::|:|\\[|\\]"; /*modified by me */
         /* forse bisogna aggiungere qui la regex per le binary expr*/
 		regEx = identRegEx + "|" + numRegEx + "|" + skipRegEx + "|" + symbolRegEx;
 	}
+/*li
+operatori unari prefissi ! , opt , empty , def e get Tutti gli operatori unari prefissi hanno la precedenza sugli operatori
+binari infissi.*/
+
 
 	static {
 		keywords.put("for", FOR);
