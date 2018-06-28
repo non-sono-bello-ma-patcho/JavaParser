7a4853857cc07a98667a52ab53facb22578361d8
rollingflamingo <andrea.storace.uni@gmail.com>
2018-06-28 18:43:33 +0200

    added constructor

--- src/Parser/ast/Empty.java
+++ src/Parser/ast/Empty.java
@@ -3,6 +3,8 @@ package Parser.ast;
 import visitors.Visitor;
 
 public class Empty extends UnaryOp {
+    public Empty(Exp exp){ super(exp);}
+
     @Override
     public <T> T accept(Visitor<T> visitor) {
         return null;
